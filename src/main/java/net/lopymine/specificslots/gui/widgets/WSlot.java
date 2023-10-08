2d1f7a91165f31ac90c3b33030adf24297aff7b6
HiWord9 <miriklevshenkov@gmail.com>
2023-10-08 07:55:23 +0300

    add ability to move items in SpecificGui

--- src/main/java/net/lopymine/specificslots/gui/widgets/WSlot.java
+++ src/main/java/net/lopymine/specificslots/gui/widgets/WSlot.java
@@ -1,5 +1,6 @@
 package net.lopymine.specificslots.gui.widgets;
 
+import io.github.cottonmc.cotton.gui.widget.WPanel;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
@@ -28,6 +29,11 @@ public class WSlot extends WWidget {
     private Item item = Items.AIR;
     private int depth = 1;
 
+    private boolean dragging = false;
+    private int draggingButton = -1;
+    private int dragX = 0;
+    private int dragY = 0;
+
     public WSlot(int index) {
         this.index = index;
     }
@@ -43,12 +49,16 @@ public class WSlot extends WWidget {
         if (isArmor()) return InputResult.IGNORED;
         MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.0F));
 
-        if (button == 1) {
-            this.resetSlot();
-            this.onToggle(false);
+        if (dragging) {
+            dragging = false;
         } else {
-            this.isOn = !this.isOn;
-            this.onToggle(this.isOn);
+            if (button == 1) {
+                this.resetSlot();
+                this.onToggle(false);
+            } else {
+                this.isOn = !this.isOn;
+                this.onToggle(this.isOn);
+            }
         }
 
         return InputResult.PROCESSED;
@@ -78,12 +88,69 @@ public class WSlot extends WWidget {
                 ScreenDrawing.texturedRect(context, x + 1, y + 1, 16, 16, ArmorSlot.getTexture(this.armorSlotType), 0xFFFFFFFF);
             }
         } else if (item != null) {
-            context.drawItem(item.getDefaultStack(), x + 1, y + 1);
+            if (!dragging || draggingButton == 2) {
+                context.drawItem(item.getDefaultStack(), x + 1, y + 1);
+            }
+            if (dragging) {
+                context.drawItem(item.getDefaultStack(), x + this.dragX, y + this.dragY);
+            }
         }
 
         if (isHovered()) {
             HandledScreen.drawSlotHighlight(context, x + 1, y + 1, 0);
         }
+    }
+
+
+    @Environment(EnvType.CLIENT)
+    @Override
+    public InputResult onMouseDrag(int x, int y, int button, double deltaX, double deltaY) {
+        if (this.item != Items.AIR) {
+            this.dragX = x;
+            this.dragY = y;
+            this.dragging = true;
+            this.draggingButton = button;
+        }
+        return InputResult.PROCESSED;
+    }
+
+    @Environment(EnvType.CLIENT)
+    @Override
+    public InputResult onMouseUp(int x, int y, int button) {
+        if (!dragging) return super.onMouseUp(x, y, button);
+        draggingButton = -1;
+
+        if (host == null) return InputResult.IGNORED;
+
+        WPanel root = host.getRootPanel();
+
+        int containerX = x + this.getAbsoluteX();
+        int containerY = y + this.getAbsoluteY();
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        assert client.currentScreen != null;
+        int screenCentreX = client.currentScreen.width / 2;
+        int screenCentreY = client.currentScreen.height / 2;
+        if ((containerX > screenCentreX + 88 || containerX < screenCentreX - 83) || (containerY > screenCentreY + 88 || containerY < screenCentreY - 83)) {
+            this.item = Items.AIR;
+        } else {
+            WWidget hit = root.hit(containerX, containerY);
+            if (hit == null) return InputResult.IGNORED;
+
+            if (hit instanceof WSlot slot) {
+                if (!slot.isArmor()) {
+                    if (button == 2) {
+                        slot.setItem(this.item);
+                    } else {
+                        Item temp = slot.item;
+                        slot.setItem(this.item);
+                        this.item = temp;
+                    }
+                }
+                return InputResult.PROCESSED;
+            }
+        }
+        return InputResult.IGNORED;
     }
 
     public WSlot setDepth(int depth) {
